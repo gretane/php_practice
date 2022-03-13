6b92409801b3fe59531fbb8fc537f3dc4011687d
gretane <greta.sufinskaite@gmail.com>
2022-03-13 13:24:07 +0200

    2 exercises

--- php_bit_ex/5_arrays_II/index.php
+++ php_bit_ex/5_arrays_II/index.php
@@ -3,25 +3,179 @@
 5 elementų su reikšmėmis nuo 5 iki 25.
 */
 echo '<h3> 1. </h3>';
+
+$m_array1 = [];
+
+// for($i = 0; $i < 10; $i++) {
+//     for($j = 0; $j < 5; $j++) {
+//         $m_array1[$i][$j] = rand(5, 25);
+//     }
+// }
+
+
+foreach(range(0, 9) as $key) {
+    foreach(range(0, 4) as $key1) {
+        $m_array1[$key][$key1] = rand(5, 25);
+    }
+}
+
+echo 'm_array1 <pre>';
+print_r($m_array1);
+echo '</pre>';
+
+
 /*
 2. Naudodamiesi 1 uždavinio masyvu:
 a) Suskaičiuokite kiek masyve yra elementų didesnių už 10;
+*/
+echo '<h3> 2. </h3>';
+
+echo '<h4> 2.a) </h4>';
+
+$count_el = 0;
+
+foreach($m_array1 as $key => $value) {
+    foreach($m_array1[$key] as $value1) {
+        if ($value1 > 10) {
+            $count_el++;
+        }
+    }
+}
+
+echo "There are $count_el elements higher than 10 in m_array1. <br/>";
+
+/*
 b) Raskite didžiausio elemento reikšmę;
+*/
+
+echo '<h4> 2.b) </h4>';
+
+// foreach($m_array1 as $key => $value) {
+//     foreach($m_array1[$key] as $key1 => &$value1) {
+//         if ($key == 0 && $key1 == 0) {
+//             continue;
+//         } else {
+//             if ($value1 < $m_array1[$key][$key1 - 1]) {
+//                 $temp = $value1;
+//                 $value1 = 
+//                 $m_array1[$key][$key1 - 1] = 
+//             }
+//         }
+//     }
+// }
+
+$m_array1b = [...$m_array1];
+
+foreach($m_array1b as $key => $value) {
+    rsort($m_array1b[$key]);
+
+}
+
+// echo 'Sorted m_array1b <pre>';
+// print_r($m_array1b);
+// echo '</pre>';
+
+$highest = $m_array1b[0][4];
+
+for($i = 1; $i < sizeof($m_array1b); $i++) {
+
+        if ($m_array1b[$i][0] > $highest) {
+            $highest = $m_array1b[$i][0];
+        }
+}
+
+
+echo "The highest number is $highest. <br/>";
+
+/*
 c) Suskaičiuokite kiekvieno antro lygio masyvų su vienodais
 indeksais sumas (t.y. suma reikšmių turinčių indeksą 0, 1 ir t.t.)
-d) Visus masyvus “pailginkite” iki 7 elementų
+*/
+
+echo '<h4> 2.c) </h4>';
+
+$sums = [];
+
+foreach(range(0, 4) as $key2) {
+    $sums[$key2] = 0;
+}
+
+// echo 'Sums: <pre>';
+// print_r($sums);
+// echo '</pre>';
+
+for($i = 0; $i < sizeof($m_array1); $i++) {
+    for ($j = 0; $j < sizeof($m_array1[$i]); $j++) { 
+        switch($j) {
+            case 0:
+                $sums[$j] += $m_array1[$i][$j]; 
+                break;
+            case 1:
+                $sums[$j] += $m_array1[$i][$j]; 
+                break;
+            case 2:
+                $sums[$j] += $m_array1[$i][$j]; 
+                break;
+            case 3:
+                $sums[$j] += $m_array1[$i][$j]; 
+                break;
+            case 4:
+                $sums[$j] += $m_array1[$i][$j]; 
+                break;
+            default:
+               echo "Something went wrong. <br/>";
+        }
+        
+    }
+}
+
+echo 'Sums: <pre>';
+print_r($sums);
+echo '</pre>';
+
+/*d) Visus masyvus “pailginkite” iki 7 elementų
+*/
+
+echo '<h4> 2.d) </h4>';
+
+for($i = 0; $i < sizeof($m_array1); $i++) {
+    for($j = 0; $j < 7; $j++) {
+        if ($j > 4) {
+            $rand_number = rand(5, 25);
+            array_push($m_array1[$i], $rand_number);
+        }  
+    }
+}
+
+echo 'm_array1[] with added elements: <pre>';
+print_r($m_array1);
+echo '</pre>';
+
+/*
 e) Suskaičiuokite kiekvieno iš antro lygio masyvų elementų sumą
 atskirai ir sumas panaudokite kaip reikšmes sukuriant naują
 masyvą. T.y. pirma naujo masyvo reikšmė turi būti lygi mažesnio
 masyvo, turinčio indeksą 0 dideliame masyve, visų elementų
 sumai.
 */
-echo '<h3> 2. </h3>';
-echo '<h4> 2.a) </h4>';
-echo '<h4> 2.b) </h4>';
-echo '<h4> 2.c) </h4>';
-echo '<h4> 2.d) </h4>';
+
 echo '<h4> 2.e) </h4>';
+
+$sums_e = [];
+
+foreach(range(0, 9) as $key_e) {
+    $sums_e[$key_e] = 0;
+}
+
+for ($i = 0; $i < sizeof($m_array1) ; $i++) {
+    for ($j = 0; $j < sizeof($m_array1[$i]); $j++) { 
+        $sums_e[$i] += $m_array1[$i][$j]; 
+    }
+}
+
+echo 'Sums of arrays\' element values: <pre>';
+print_r($sums_e);
+echo '</pre>';
 
 /*
 3. Sukurkite masyvą iš 10 elementų. Kiekvienas masyvo elementas turi
