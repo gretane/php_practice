da183e0ddd85eb5a91f2164c945f7b81412bcab7
gretane <greta.sufinskaite@gmail.com>
2022-03-14 15:54:33 +0200

    4. only arrays with "K" sorted

--- php_bit_ex/5_arrays_II/index.php
+++ php_bit_ex/5_arrays_II/index.php
@@ -219,6 +219,119 @@ kurių masyvai trumpiausi eitų pradžioje. Masyvai kurie turi bent vieną
 
 echo '<h3> 4. </h3>';
 
+$sizes = [];
+$i = 0;
+
+
+foreach(range(0, sizeof($m_array3) -1) as $key_sizes) {
+    $sizes[$key_sizes] = sizeof($m_array3[$i]);
+    $i++;
+}
+
+echo 'Sizes: <pre>';
+print_r($sizes);
+echo '</pre>';
+
+$k_letter = [];
+$i = 0;
+
+foreach(range(0, sizeof($m_array3) -1) as $key_k) {
+    $k_letter[$key_k] = array_search("K", $m_array3[$i]);
+    $i++;
+}
+
+echo 'K_letter: <pre>';
+print_r($k_letter);
+echo '</pre>';
+
+$m_array3_k = [];
+
+for($i = 0; $i < sizeof($m_array3); $i++) {
+
+    if (array_search("K", $m_array3[$i]) || array_search("K", $m_array3[$i]) === 0) {
+        array_push($m_array3_k, $m_array3[$i]);
+    }
+}
+
+echo 'm_array3_k only with k without sort: <pre>';
+print_r($m_array3_k);
+echo '</pre>';
+
+$smallest_size_with_k = sizeof($m_array3_k[0]);
+$index = 0;
+
+for($i = 0; $i < sizeof($m_array3_k); $i++) {              // while (sizeof $m_array3_k < 0)
+    if (sizeof($m_array3_k[$i]) <= $smallest_size_with_k) {
+        $smallest_size_with_k = sizeof($m_array3_k[$i]);
+        $index = $i;
+    } 
+}
+
+echo "The smallest array in m_array3_k is with index: $index. Size: $smallest_size_with_k. <br/><br/>";
+
+// array_unshift($m_array3_k, $m_array3_k[$index]);
+
+// echo 'm_array3_k smallest with k at front: <pre>';
+// print_r($m_array3_k);
+// echo '</pre>';
+
+// unset($m_array3_k[$index + 1]);
+
+// echo 'm_array3_k smallest with k at front and $index + 1 unset: <pre>';
+// print_r($m_array3_k);
+// echo '</pre>';
+
+$k_sort = [];
+$temp = []; 
+
+while (sizeof($m_array3_k) > 0) {
+    $smallest_size_with_k = sizeof($m_array3_k[0]);
+    $index = 0;
+    for($i = 0; $i < sizeof($m_array3_k); $i++) {             
+        if (sizeof($m_array3_k[$i]) <= $smallest_size_with_k) {
+            $smallest_size_with_k = sizeof($m_array3_k[$i]);
+            $index = $i;
+        } 
+    }
+    array_push($k_sort, $m_array3_k[$index]);
+    unset($m_array3_k[$index]);
+
+    if (sizeof($m_array3_k) > 0) {
+        $temp = [...$m_array3_k];
+        $m_array3_k = [...$temp];
+    }  
+
+}
+
+
+
+echo 'k_sort: <pre>';
+print_r($k_sort);
+echo '</pre>';
+
+echo 'm_array_3_k without elements: <pre>';
+print_r($m_array3_k);
+echo '</pre>';
+
+// echo 'm_array3: <pre>';
+// print_r($m_array3);
+// echo '</pre>';
+
+
+// echo 'm_array3: <pre>';
+// print_r($m_array3);
+// echo '</pre>';
+
+
+// if (sizeof($m_array3[1]) < sizeof($m_array3[1 - 1])) {
+//     array_unshift($m_array3, $m_array3[1]);
+//     unset($m_array3[1 + 1]);
+// }
+
+
+//in_array("K", $m_array3[]);
+
+
 
 /*
 5. Sukurkite masyvą iš 30 elementų. Kiekvienas masyvo elementas yra
